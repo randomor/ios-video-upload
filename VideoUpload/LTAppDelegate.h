94e6c3b7011deabb7970df5f3cc24c99cb4414dc
randomor <randomor@gmail.com>
2012-05-20 00:59:49 -0500

    Initial Commit

---
+++ VideoUpload/LTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LTAppDelegate.h
+//  VideoUpload
+//
+//  Created by Shaomeng Zhang on 5/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ VideoUpload/LTViewController.h
@@ -0,0 +1,13 @@
+//
+//  LTViewController.h
+//  VideoUpload
+//
+//  Created by Shaomeng Zhang on 5/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LTViewController : UIViewController
+
+@end
